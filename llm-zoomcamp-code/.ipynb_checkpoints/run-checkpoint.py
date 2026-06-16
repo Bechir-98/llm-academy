f91f358b38c39f842109d01919ad08e89b4af6d7
Bechir-98 <cilebre@hotmail.com>
2026-06-16 21:42:50 +0100

    use persistent rag

---
+++ llm-zoomcamp-code/.ipynb_checkpoints/run-checkpoint.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from ingest import load_faq_data, build_index
+from rag_helper import RAGBase
+from openai import OpenAI
+
+documents = load_faq_data()
+index = build_index(documents)
+
+openai_client =OpenAI(
+    base_url="https://api.groq.com/openai/v1",
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+assistant = RAGBase(
+    index=index,
+    llm_client=openai_client,
+)
+
+answer = assistant.rag("I just discovered the course. Can I join now?")
+print(answer)
